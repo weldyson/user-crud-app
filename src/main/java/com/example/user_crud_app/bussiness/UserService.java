2d942ad018e69bea21f86b41c08fd0cba717031b
Weldyson de Azevedo <41272112+weldyson@users.noreply.github.com>
2025-10-28 22:38:45 -0300

    Add find and delete user by email methods
    Introduced methods in UserService and UserRepository to find and delete users by email. This enhances user management capabilities by allowing lookup and removal of users using their email addresses.

--- src/main/java/com/example/user_crud_app/bussiness/UserService.java
+++ src/main/java/com/example/user_crud_app/bussiness/UserService.java
@@ -9,12 +9,23 @@ public class UserService {
 
     private final UserRepository repository;
 
-    public UserService (UserRepository repository) {
+    public UserService(UserRepository repository) {
         this.repository = repository;
     }
 
-    public void saveUser(User user){
+    public void saveUser(User user) {
         repository.saveAndFlush(user);
     }
+
+
+    public User findUserByEmail(String email){
+        return  repository.findByEmail(email).orElseThrow(
+                () -> new RuntimeException("Email não encontrado"));
+    }
+
+    public void deleteUserByEmail(String email){
+        repository.deleteByEmail(email);
+    }
+
+
 }
-//13:58
--- src/main/java/com/example/user_crud_app/infrastructure/repository/UserRepository.java
+++ src/main/java/com/example/user_crud_app/infrastructure/repository/UserRepository.java
@@ -1,9 +1,17 @@
 package com.example.user_crud_app.infrastructure.repository;
 
 import com.example.user_crud_app.infrastructure.entitys.User;
+import jakarta.transaction.Transactional;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface UserRepository  extends JpaRepository<User,Integer>{
+    Optional<User> findByEmail(String email);
+
+
+    @Transactional
+    void deleteByEmail(String email);
 
 }
 
